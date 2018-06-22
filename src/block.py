c86f502ae9634786d9bcfa19c860389692bf99c3
Robert Rak <robertrak@hotmail.com>
2018-06-22 14:14:02 -0400

    initial

--- src/block.py
+++ src/block.py
@@ -11,6 +11,8 @@ class Block:
     def __init__(self, position, width, height, color):
         # Create a rectangle centered around the x and y
         self.position = position
+        self.width = width
+        self.height = height
         self.rectangle = pygame.Rect(
                                     position.x - (width/2),
                                     position.y - (height/2),
@@ -34,4 +36,26 @@ class KineticBlock(Block):
     # KineticBall will handle the collison
     pass
 
+class Paddle(KineticBlock):
+    def update(self):
+        self.rectangle = pygame.Rect(
+                                    self.position.x - (self.width/2),
+                                    self.position.y - (self.height/2),
+                                    self.width,
+                                    self.height)
 
+class SingleHitBlock(KineticBlock):
+    def __init__(self, position, width, height, color):
+        super().__init__(position, width, height, color)
+        self.hitpoints = 1
+
+    def check_collision(self):
+        if self.touched_by_ball == True:
+            self.hitpoints -= 1
+            print(self.hitpoints)
+        if self.hitpoints <= 0:
+            self.__del__(self)
+
+class MultiHitBlock(SingleHitBlock):
+    def __init__(self):
+        self.hitpoints = 3
