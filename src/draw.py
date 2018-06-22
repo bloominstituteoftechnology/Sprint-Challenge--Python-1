cacb1ef40e08d3a53d3a2854c788e6945638695d
unknown <luisd824@gmail.com>
2018-06-22 13:44:15 -0400

    change screen size, ball size, attempt to make grid of blocks

--- src/draw.py
+++ src/draw.py
@@ -1,4 +1,4 @@
-import pygame #TODO:  Fix intellisense
+import pygame  # TODO:  Fix intellisense
 import random
 
 from pygame.math import Vector2
@@ -6,34 +6,45 @@ from pygame.math import Vector2
 from ball import *
 from block import *
 
-SCREEN_SIZE = [640, 480]
+SCREEN_SIZE = [400, 800]
 BACKGROUND_COLOR = [255, 255, 255]
+BLOCK_COUNT = 10
+BLOCK_X = 10
+BLOCK_Y = 10
+
 
 def debug_create_objects(object_list):
-    kinetic = GameBall(1, object_list, SCREEN_SIZE, 
-                                    Vector2(random.randint(20, SCREEN_SIZE[0] - 20), random.randint(20, SCREEN_SIZE[1] - 20)),
-                                    Vector2(4*random.random() - 2, 4*random.random() - 2),
-                                    [255, 10, 0], 20)
+    kinetic = GameBall(1, object_list, SCREEN_SIZE,
+                       Vector2(random.randint(
+                           20, SCREEN_SIZE[0] - 20), random.randint(20, SCREEN_SIZE[1] - 20)),
+                       Vector2(4*random.random() - 2, 4*random.random() - 2),
+                       [255, 10, 0], 10)
     object_list.append(kinetic)
+    top = 0
+    for row in range(5):
+        for column in range(5):
+            top += 10
+            block = KineticBlock(
+                Vector2((column * 50) + 2, top), 50, 15, [0, 0, 255])
+            object_list.append(block)
 
-    block = KineticBlock(Vector2(200,200), 100, 100, [0, 0, 255])
-    object_list.append(block)
-  
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
- 
+
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
- 
-    object_list = [] # list of objects of all types in the toy
-    
+
+    object_list = []  # list of objects of all types in the toy
+
     debug_create_objects(object_list)
- 
-    while True: # TODO:  Create more elegant condition for loop
+
+    while True:  # TODO:  Create more elegant condition for loop
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-        
+            if event.type == pygame.QUIT:
+                sys.exit()
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             # Do something
@@ -45,17 +56,18 @@ def main():
         for object in object_list:
             object.update()
             object.check_collision()
- 
+
         # Draw Updates
         screen.fill(BACKGROUND_COLOR)
         for ball in object_list:
             ball.draw(screen, pygame)
- 
+
         clock.tick(60)
         pygame.display.flip()
- 
+
     # Close everything down
     pygame.quit()
- 
+
+
 if __name__ == "__main__":
     main()
