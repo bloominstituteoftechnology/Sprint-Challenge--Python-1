34c3aaffd8db253f4d01eed70ca3c8f3a2de5824
Edward Manda <lilkups@yahoo.com>
2018-06-22 11:30:07 -0500

    added a paddle

--- src/draw.py
+++ src/draw.py
@@ -1,4 +1,4 @@
-import pygame #TODO:  Fix intellisense
+import pygame  # TODO:  Fix intellisense
 import random
 
 from pygame.math import Vector2
@@ -9,31 +9,40 @@ from block import *
 SCREEN_SIZE = [640, 480]
 BACKGROUND_COLOR = [255, 255, 255]
 
+
 def debug_create_objects(object_list):
-    kinetic = GameBall(1, object_list, SCREEN_SIZE, 
-                                    Vector2(random.randint(20, SCREEN_SIZE[0] - 20), random.randint(20, SCREEN_SIZE[1] - 20)),
-                                    Vector2(4*random.random() - 2, 4*random.random() - 2),
-                                    [255, 10, 0], 20)
+    kinetic = GameBall(1, object_list, SCREEN_SIZE,
+                       Vector2(random.randint(
+                           20, SCREEN_SIZE[0] - 20), random.randint(20, SCREEN_SIZE[1] - 20)),
+                       Vector2(4*random.random() - 2, 4*random.random() - 2),
+                       [255, 10, 0], 20)
+
     object_list.append(kinetic)
 
-    block = KineticBlock(Vector2(200,200), 100, 100, [0, 0, 255])
+    block = KineticBlock(Vector2(200, 200), 100, 100, [0, 0, 255])
     object_list.append(block)
-  
+
+    # my actions
+    paddle = KineticBlock(Vector2(240, 470), 80, 10, [0, 255, 0])
+    object_list.append(paddle)
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
- 
+
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
- 
-    object_list = [] # list of objects of all types in the toy
-    
+
+    object_list = []  # list of objects of all types in the toy
+
     debug_create_objects(object_list)
- 
-    while True: # TODO:  Create more elegant condition for loop
+
+    while True:  # TODO:  Create more elegant condition for loop
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-        
+            if event.type == pygame.QUIT:
+                sys.exit()
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             # Do something
@@ -45,17 +54,18 @@ def main():
         for object in object_list:
             object.update()
             object.check_collision()
- 
+
         # Draw Updates
         screen.fill(BACKGROUND_COLOR)
         for ball in object_list:
             ball.draw(screen, pygame)
- 
+
         clock.tick(60)
         pygame.display.flip()
- 
+
     # Close everything down
     pygame.quit()
- 
+
+
 if __name__ == "__main__":
     main()
