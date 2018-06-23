78cbb3b0c21ad4f3c696e5f20efa81982daf6a64
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-06-23 19:22:59 -0400

    Sprint Complete

--- src/draw.py
+++ src/draw.py
@@ -7,8 +7,10 @@ from ball import *
 from block import *
 
 SCREEN_SIZE = [400, 800]
-BACKGROUND_COLOR = [0, 0, 0]
-GAME_BLOCK_COLOR = [150,150,150]
+BACKGROUND_COLOR = [245, 245, 245]
+PADDLE_COLOR = [179,120,211]
+BLOCK_COLOR = [0,0,0]
+BALL_COLOR = [210,105,30]
 
 # TODO: random RGB color generator for Blocks to be Destroyed
 def rgb_color():
@@ -17,40 +19,43 @@ def rgb_color():
 
 
 def debug_create_objects(object_list):
+    
     kinetic = GameBall(1, object_list, 
                                     SCREEN_SIZE, # bounds
                                     # randomized starting position 
                                     Vector2(random.randint(30, SCREEN_SIZE[0] - 10), random.randint(10, SCREEN_SIZE[1] - 10)),
                                     # velocity
                                     # Two random numbers between -2 and 2 -- Why would you randomize the velocity?
-                                    Vector2(-2, 2), # was `4*random.random() - 2`
-                                    rgb_color(), # color
+                                    Vector2(-8, 8), # was `4*random.random() - 2`
+                                    BALL_COLOR, # color
                                     10) # radius
     object_list.append(kinetic)
 
-    # User Block
-    block = KineticBlock(Vector2(200,790), 100, 20, rgb_color())
-    object_list.append(block)
+    for i in range(5):
+        block=StrongBlock(3, Vector2(20 + i * 80, 20), 40, 40, BLOCK_COLOR)
+        object_list.append(block)
+    # for i in range(5):
+    #     block=StrongBlock(3, Vector2(60 + i * 80, 60), 40, 40, BLOCK_COLOR)
+    #     object_list.append(block)
+    # for i in range(5):
+    #     block=StrongBlock(3, Vector2(20 + i * 80, 100), 40, 40, BLOCK_COLOR)
+    #     object_list.append(block)
+    # for i in range(5):
+    #     block=StrongBlock(3, Vector2(60 + i * 80, 140), 40, 40, BLOCK_COLOR)
+    #     object_list.append(block)
+    # for i in range(5):
+    #     block=StrongBlock(3, Vector2(20 + i * 80, 180), 40, 40, BLOCK_COLOR)
+    #     object_list.append(block)
+    # for i in range(5):
+    #     block=StrongBlock(3, Vector2(60 + i * 80, 220), 40, 40, BLOCK_COLOR)
+    #     object_list.append(block)
+            
 
-    # Blocks to be Destroyed by User
-    dblock_1 = KineticBlock(Vector2(25,10), 40, 20, GAME_BLOCK_COLOR)
-    object_list.append(dblock_1)
-    dblock_2 = KineticBlock(Vector2(75,10), 40, 20, GAME_BLOCK_COLOR)
-    object_list.append(dblock_2)
-    dblock_3 = KineticBlock(Vector2(125,10), 40, 20, GAME_BLOCK_COLOR)
-    object_list.append(dblock_3)
-    dblock_4 = KineticBlock(Vector2(175,10), 40, 20, GAME_BLOCK_COLOR)
-    object_list.append(dblock_4)
-    dblock_5 = KineticBlock(Vector2(225,10), 40, 20, GAME_BLOCK_COLOR)
-    object_list.append(dblock_5)
-    dblock_6 = KineticBlock(Vector2(275,10), 40, 20, GAME_BLOCK_COLOR)
-    object_list.append(dblock_6)
-    dblock_7 = KineticBlock(Vector2(325,10), 40, 20, GAME_BLOCK_COLOR)
-    object_list.append(dblock_7)
-    dblock_8 = KineticBlock(Vector2(375,10), 40, 20, GAME_BLOCK_COLOR)
-    object_list.append(dblock_8)
-
-
+    paddle = Paddle(Vector2(SCREEN_SIZE[0]/2, SCREEN_SIZE[1]-50),
+                         200, 
+                         20, 
+                         PADDLE_COLOR)
+    object_list.append(paddle)
   
 def main():
     pygame.init()
@@ -63,32 +68,38 @@ def main():
     
     debug_create_objects(object_list)
  
-    while True: # TODO:  Create more elegant condition for loop
+    while len(object_list) > 2: # TODO:  Create more elegant condition for loop
+        right = False
+        left = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
         
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            print(object_list[1].position[0])
-            object_list[1].position[0] += 1
-            print(object_list[1].position[0])
+            left = True
+            # print(object_list[1].position[0])
+            # object_list[1].position[0] += 1
+            # print(object_list[1].position[0])
+            pass
             
             
             
         if keys[pygame.K_RIGHT]:
+            right = True
             # Do something
             # print(object_list[0]) # ball.GameBall object x 8
             # print(object_list[1]) # block.KineticBlock object x 1
             # print(object_list[2]) # block.KineticBlock object x 1
             # print(len(object_list)) # 10... 1 ball, 1 user block, 8 blocks to destroy
-            print(object_list[1].position[0])
-            object_list[1].position[0] -= 1
-            print(object_list[1].position[0])
+            # print(object_list[1].position[0])
+            # object_list[1].position[0] -= 1
+            # print(object_list[1].position[0])
             # TODO: Figure out how to implement super().update() .. like in OOP-Toy            
-            
+            pass
 
         for object in object_list:
-            object.update()
+            object.update(left= left, right=right, pygame = pygame, object_list = object_list)
             object.check_collision()
  
         # Draw Updates
