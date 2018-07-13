d8546f945ce8f6e4bd9f54e260610f7868115108
Tramane Hall <tramanedhall@gmail.com>
2018-07-13 13:12:07 -0400

    created paddle and two kinds of blocks

--- src/block.py
+++ src/block.py
@@ -33,5 +33,46 @@ class KineticBlock(Block):
     # No custom code needed here, just want to be able to differentiate
     # KineticBall will handle the collison
     pass
+    
+class Paddle(KineticBlock)
+    SPEED = 4
+
+    def update(self, **input):
+        self.left = input['left']
+        self.right = inmput['right']
+
+        if self.left:
+            self.position.x -= self.SPEED
+        
+        if self.right:
+        self.position.x += self.SPEED
+
+        self.rectangle = pygame.Rect(
+            self.position.x - (self.rectangle.width/2),
+            self.position.y - (self.rectangle.height/2),
+            self.rectangle.width,
+            self.rectangle.height
+        )
+        super().update()
+
+class EasyBlock(KineticBlock):
+    def update(self, **kwargs):
+        if self.touched_by_ball:
+            kwargs['object_list'].pop(kwargs['object_list'].index(self))
 
 
+class HardBlock(EasyBlock):
+    def __init__(self, life, position, width, height, color):
+        self.life = life
+        super().__init__(position, width, height, color)
+
+    random_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0,255)]
+
+    def update(self, **kwargs):
+        if self.touched_by_ball:
+            self.life -= 1
+            self.color = self.random_color
+            if self.life <= 0:
+                super().update(object_list = kwargs['object_list'])
+            else:
+                self.touched_by_ball = False
