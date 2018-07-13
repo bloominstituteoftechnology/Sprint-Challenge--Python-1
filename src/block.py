c538a3aeb5cdd2b92b709285b590b9835151f0ec
Danny Vail <danny_vail@outlook.com>
2018-07-13 12:04:19 -0400

    Initial commit - converted code in draw.py to a GameManager class for readability - minor formatting edits to ball.py and block.py

--- src/block.py
+++ src/block.py
@@ -3,22 +3,21 @@ import pygame
 from pygame.math import Vector2
 from pygame import Rect
 
+
 class Block:
     """
     Base class for square or rectangular object
     """
 
     def __init__(self, position, width, height, color):
-        # Create a rectangle centered around the x and y
         self.position = position
         self.rectangle = pygame.Rect(
-                                    position.x - (width/2),
-                                    position.y - (height/2),
-                                    width,
-                                    height)
+            position.x - (width/2),
+            position.y - (height/2),
+            width,
+            height)
         self.color = color
         self.touched_by_ball = False
-
 
     def update(self, **kwargs):
         self.touched_by_ball = False
@@ -29,9 +28,10 @@ class Block:
     def draw(self, screen, pygame):
         pygame.draw.rect(screen, self.color, self.rectangle)
 
+
 class KineticBlock(Block):
-    # No custom code needed here, just want to be able to differentiate
+    """
+    Block that is collidable
+    """
     # KineticBall will handle the collison
     pass
-
-
--- src/draw.py
+++ src/draw.py
@@ -1,62 +1,67 @@
-import pygame #TODO:  Fix intellisense
+import pygame
 import random
+import sys
 
 from pygame.math import Vector2
+from ball import GameBall
+from block import KineticBlock
 
-from ball import *
-from block import *
 
-SCREEN_SIZE = [640, 480]
-BACKGROUND_COLOR = [255, 255, 255]
+class GameManager:
+    def __init__(self):
+        self.SCREEN_SIZE = [640, 480]
+        self.BACKGROUND_COLOR = [255, 255, 255]
+        self.object_list = []
 
-def debug_create_objects(object_list):
-    kinetic = GameBall(1, object_list, SCREEN_SIZE, 
-                                    Vector2(random.randint(20, SCREEN_SIZE[0] - 20), random.randint(20, SCREEN_SIZE[1] - 20)),
-                                    Vector2(4*random.random() - 2, 4*random.random() - 2),
-                                    [255, 10, 0], 20)
-    object_list.append(kinetic)
+    def build_level(self):
+        # TODO: Create paddle, blocks and ball
+        kinetic = GameBall(1, self.object_list, self.SCREEN_SIZE,
+                           Vector2(random.randint(
+                               20, self.SCREEN_SIZE[0] - 20), random.randint(20, self.SCREEN_SIZE[1] - 20)),
+                           Vector2(4*random.random() - 2,
+                                   4*random.random() - 2),
+                           [255, 10, 0], 20)
+        block = KineticBlock(Vector2(200, 200), 100, 100, [0, 0, 255])
+        self.object_list.append(kinetic)
+        self.object_list.append(block)
 
-    block = KineticBlock(Vector2(200,200), 100, 100, [0, 0, 255])
-    object_list.append(block)
-  
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode(SCREEN_SIZE)
- 
-    # Used to manage how fast the screen updates
-    clock = pygame.time.Clock()
- 
-    object_list = [] # list of objects of all types in the toy
-    
-    debug_create_objects(object_list)
- 
-    while True: # TODO:  Create more elegant condition for loop
-        left = False
-        right = False
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-        
-        #TODO:  Feed input variables into update for objects that need it.
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            left = True
-        if keys[pygame.K_RIGHT]:
-            right = True
-        for object in object_list:
-            object.update()
-            object.check_collision()
- 
-        # Draw Updates
-        screen.fill(BACKGROUND_COLOR)
-        for ball in object_list:
-            ball.draw(screen, pygame)
- 
-        clock.tick(60)
-        pygame.display.flip()
- 
-    # Close everything down
-    pygame.quit()
- 
+    def start_game(self):
+        screen = pygame.display.set_mode(self.SCREEN_SIZE)
+        clock = pygame.time.Clock()
+
+        while True:
+            left = False
+            right = False
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #TODO:  Feed input variables into update for objects that need it.
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                left = True
+            if keys[pygame.K_RIGHT]:
+                right = True
+            for object in self.object_list:
+                object.update()
+                object.check_collision()
+
+            # Draw Updates
+            screen.fill(self.BACKGROUND_COLOR)
+            for ball in self.object_list:
+                ball.draw(screen, pygame)
+
+            clock.tick(60)
+            pygame.display.flip()
+
+    def main(self):
+        pygame.init()
+        self.build_level()
+        self.start_game()
+        pygame.quit()
+
+
 if __name__ == "__main__":
-    main()
+    breakout = GameManager()
+    breakout.main()
