4f10819654af1824e526b93bdce01ac6f97ec548
Matthias Bakken <matthias.bakken@gmail.com>
2018-07-13 09:55:29 -0700

    added blocks. No functionality as of yet.

--- src/draw.py
+++ src/draw.py
@@ -6,18 +6,23 @@ from pygame.math import Vector2
 from ball import *
 from block import *
 
-SCREEN_SIZE = [640, 480]
+SCREEN_SIZE = [640, 700]
 BACKGROUND_COLOR = [255, 255, 255]
 
 def debug_create_objects(object_list):
     kinetic = GameBall(1, object_list, SCREEN_SIZE, 
                                     Vector2(300, 300),
-                                    Vector2(4*random.random() - 2, 4*random.random() - 2),
+                                    Vector2(7, -7),
                                     [255, 10, 0], 20)
     object_list.append(kinetic)
 
-    block = KineticBlock(Vector2(200,200), 100, 100, [0, 0, 255])
-    object_list.append(block)
+    for i in range(5):
+       block_list = []
+       color = [34,100,25]
+       for j in range(4):
+           block = KineticBlock(Vector2(72 + (i*124),100 + (j* 40)), 120, 30, color, object_list)
+           object_list.append(block)
+           print(object_list)
   
 def main():
     pygame.init()
