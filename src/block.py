4349be9dec1a43cabd19af2998d0bafde17c4bb4
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-13 08:56:25 -0700

    paddle class created and an instance added to the o

--- src/block.py
+++ src/block.py
@@ -8,7 +8,7 @@ class Block:
     Base class for square or rectangular object
     """
 
-    def __init__(self, position, width, height, color):
+    def __init__(self, position, width, height, color, object_list):
         # Create a rectangle centered around the x and y
         self.position = position
         self.rectangle = pygame.Rect(
@@ -34,4 +34,41 @@ class KineticBlock(Block):
     # KineticBall will handle the collison
     pass
 
+class Paddle(KineticBlock):
+    def __init__(self, position, width, height, color, object_list):
+        self.left = False
+        self.right = False
+        self.height = height
+        self.width = width
+        super().__init__(position, width, height, color, object_list)
+
+
+    def update(self):
+        if self.touched_by_ball == True:
+            self.color = [0,0,62]
+
+        self.touched_by_ball = False
+        if self.left == True:
+            self.position.x = (self.position.x - 3) if self.position.x > 30 else 30 #TODO: dynamic min
+            self.rectangle = pygame.Rect(
+                                    self.position.x - (self.width/2),
+                                    self.position.y - (self.height/2),
+                                    self.width,
+                                    self.height)
+        if self.right == True:
+            self.position.x = (self.position.x + 3) if self.position.x < 370 else 370 #TODO: dynamic max
+            self.rectangle = pygame.Rect(
+                                    self.position.x - (self.width/2),
+                                    self.position.y - (self.height/2),
+                                    self.width,
+                                    self.height)
+        self.left = False
+        self.right = False
+        super().update()
+    
+    def move_left(self):
+        self.left = True
+
+    def move_right(self):
+        self.right = True
 
