5239fccd9f53ccb21aa38a25b2a58e31eebd7bb5
FireInjun <mk@fireinjun.com>
2018-07-13 12:41:44 -0400

    Had serious issues setting up the environment but am currently running and working.

--- src/draw.py
+++ src/draw.py
@@ -18,18 +18,18 @@ def debug_create_objects(object_list):
 
     block = KineticBlock(Vector2(200,200), 100, 100, [0, 0, 255])
     object_list.append(block)
-  
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
- 
+
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
- 
+
     object_list = [] # list of objects of all types in the toy
     
     debug_create_objects(object_list)
- 
+
     while True: # TODO:  Create more elegant condition for loop
         left = False
         right = False
@@ -46,17 +46,17 @@ def main():
         for object in object_list:
             object.update()
             object.check_collision()
- 
+
         # Draw Updates
         screen.fill(BACKGROUND_COLOR)
         for ball in object_list:
             ball.draw(screen, pygame)
- 
+
         clock.tick(60)
         pygame.display.flip()
- 
+
     # Close everything down
     pygame.quit()
- 
+
 if __name__ == "__main__":
     main()
