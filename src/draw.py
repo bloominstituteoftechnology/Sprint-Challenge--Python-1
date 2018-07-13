11ea031c46c1252df948f9ac8398631fc5d6d2d6
Jacob Henry <jacobiswinning@gmail.com>
2018-07-13 13:01:53 -0600

    added blocks, however they move with the paddle

--- src/draw.py
+++ src/draw.py
@@ -1,4 +1,4 @@
-import pygame #TODO:  Fix intellisense
+import pygame  # TODO:  Fix intellisense
 import random
 
 from pygame.math import Vector2
@@ -11,48 +11,64 @@ S_WIDTH = 400
 S_HEIGHT = 800
 BACKGROUND_COLOR = [255, 255, 255]
 PADDLE_WIDTH = 120
-PADDLE_HEIGHT = 20
+PADDLE_HEIGHT = 30
 
 NUMBER_OF_BLOCKS = 6
-BLOCK_WIDTH = S_WIDTH/NUMBER_OF_BLOCKS
-BLOCK_HEIGHT = BLOCK_WIDTH/2
+BLOCK_WIDTH = S_WIDTH / NUMBER_OF_BLOCKS
+BLOCK_HEIGHT = BLOCK_WIDTH / 2
+
 
 def debug_create_objects(object_list):
-    kinetic = GameBall(1, object_list, SCREEN_SIZE, 
-                                    Vector2(random.randint(20, SCREEN_SIZE[0] - 20), random.randint(20, SCREEN_SIZE[1] - 20)),
-                                    Vector2(4*random.random() - 2, 4*random.random() - 2),
-                                    [255, 10, 0], 20)
+    kinetic = GameBall(
+        1,
+        object_list,
+        SCREEN_SIZE,
+        Vector2(
+            random.randint(20, SCREEN_SIZE[0] - 20),
+            random.randint(20, SCREEN_SIZE[1] - 20),
+        ),
+        Vector2(4 * random.random() - 2, 4 * random.random() - 2),
+        [255, 10, 0],
+        20,
+    )
     object_list.append(kinetic)
 
-    block = KineticBlock(Vector2(S_WIDTH/2,S_HEIGHT-20), PADDLE_WIDTH, PADDLE_HEIGHT, [0, 0, 255])
-    
-    blocks = KineticBlock(Vector2(S_WIDTH/2,S_HEIGHT/2), S_WIDTH/NUMBER_OF_BLOCKS, BLOCK_HEIGHT, [0, 0, 255])
+    block = KineticBlock(
+        Vector2(S_WIDTH / 2, S_HEIGHT - 20), PADDLE_WIDTH, PADDLE_HEIGHT, [0, 0, 255]
+    )
+    object_list.append(block)
     # CREATE BLOCKS
 
-    # for x in range(0,int(S_WIDTH), int(BLOCK_WIDTH)):
-    #     blocks.append(block)
-    object_list.append(blocks)
-    object_list.append(block)
-  
+    for i in range(3):
+        blocks = []  # WIP
+        color = [104, 237, 126]
+        for x in range(3):
+            block = KineticBlock(
+                Vector2(72 + (i * 124), 100 + (x * 40)), 120, 30, color
+            )
+            object_list.append(block)
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
- 
+
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
- 
-    object_list = [] # list of objects of all types in the toy
-    
+
+    object_list = []  # list of objects of all types in the toy
+
     debug_create_objects(object_list)
- 
-    while True: # TODO:  Create more elegant condition for loop
+
+    while True:  # TODO:  Create more elegant condition for loop
         left = False
         right = False
-        
+
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-        
-        #TODO:  Feed input variables into update for objects that need it.
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # TODO:  Feed input variables into update for objects that need it.
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             left = True
@@ -65,7 +81,7 @@ def main():
             else:
                 object.update()
                 object.check_collision()
- 
+
         # Draw Updates
         screen.fill(BACKGROUND_COLOR)
         for ball in object_list:
@@ -74,9 +90,10 @@ def main():
         #     blocks[i].draw(display)
         clock.tick(60)
         pygame.display.flip()
- 
+
     # Close everything down
     pygame.quit()
- 
+
+
 if __name__ == "__main__":
     main()
