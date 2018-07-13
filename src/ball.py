a4d17a8fc8ef0d4ae9151c0e0ed52e1c79df8a2c
honda0306 <christopher.honda92@gmail.com>
2018-07-13 09:59:17 -0700

    Added check for ball hitting bottom of screen

--- src/ball.py
+++ src/ball.py
@@ -4,6 +4,7 @@ from pygame.math import Vector2
 from pygame import Rect
 
 from block import KineticBlock
+from block import PlayerPaddle
 
 class Ball:
     """
@@ -163,7 +164,10 @@ class GameBall(Ball):
         index = self.object_list.index(self)
         for object in self.object_list[index+1:]:  # TODO: Check effeciency
             # Balls colliding with blocks
-            if issubclass(type(object), KineticBlock) and object != self:
+            if (issubclass(type(object), KineticBlock) or issubclass(type(object), PlayerPaddle)) and object != self:
                 # Do a first round pass for collision (we know object is a KineticBlock)
                 if self.collision_rectangle.colliderect(object.rectangle):
                     self.collide_with_rectangle(object)
+
+    def ball_hit_bottom(self):
+        return self.position.y >= self.bounds[1] - self.radius
