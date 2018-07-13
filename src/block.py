a6cf1f30e92b3265c984c91bdd7a0daac8d8bcf1
kelly <kelfro@fastmail.com>
2018-07-13 11:56:27 -0700

    MVP achieved the paddle works, I have different block types, the ball bounces around, the screen is the right size, and the whole thing shuts down if you hit the bottom or the top of the screen. Whew!

--- src/block.py
+++ src/block.py
@@ -40,35 +40,35 @@ class RegularBlock(KineticBlock):
     def __init__(self, position, width, height, color, object_list):
         self.object_list = object_list
         super().__init__(position, width, height, color)
-        self.hit = 1
+        self.hit = 0
     
-    def update(self):
+    def check_collision(self):
         if self.touched_by_ball == True:
-            self.hit -= 1
-            if self.hit < 1:
-                self.object_list.remove(self)
-        super().update()
+            self.object_list.remove(self)
 
 class RainbowBlock(RegularBlock):
     def __init__(self, position, width, height, color, object_list):
         self.object_list = object_list
         super().__init__(position, width, height, color, object_list)
-        self.hit = 3
 
-    def update(self):
+    def check_collision(self):
         if self.touched_by_ball == True:
-            self.hit -= 1
-        if self.hit == 2:
-            self.color = [255, 0, 255]
-        if self.hit == 1:
-            self.color = [255, 255, 0]
-        if self.hit < 1:
-            self.object_list.remove(self)
-        super().update()
+            self.hit += 1
+            if self.hit == 1:
+                self.color = [100, 100, 100]
+            if self.hit == 2:
+                self.color = [0, 0, 0]
+            if self.hit == 3:
+                self.color = [255, 255, 0 ]
+            if self.hit == 4:
+                self.color = [255, 0, 255]
+            if self.hit >= 5:
+                self.object_list.remove(self)
 
 # The above allows the Rainbow Block to take a certain number of hits before vanishing
 # I was making this way too complicated. I tried using self.should_draw but it wasn't working so I figured the base Block has a self.touched_by_ball, so maybe I ought to use that.
 # Then maybe I could count whether it got hit and then change color once before removing. Not working 100% but I feel very close. 
+# Ok, finally working. I decided to count up instead of down, which helped. I also renamed the method to refer to the check_collision from ball.py. Not sure if that was what did the trick as I did a lot of things at the same time.
 
 class Paddle(KineticBlock):
     def __init__(self, position, width, height, color):
