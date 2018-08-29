0cc5fced567a1b1f0f5181c83eb9d69b85a6c804
Jacob Henry <jacobiswinning@gmail.com>
2018-08-29 13:26:25 -0600

    finished Pygame Breakout clone

--- src/block.py
+++ src/block.py
@@ -1,7 +1,7 @@
 import pygame
 
-from pygame.math import Vector2
-from pygame import Rect
+# from pygame.math import Vector2
+# from pygame import Rect
 
 
 class Block:
@@ -34,15 +34,44 @@ class KineticBlock(Block):
     pass
 
 
+class Breakable(KineticBlock):
+    def update(self, object_list):
+        if self.touched_by_ball:
+            for item in object_list:
+                if (
+                    item == self
+                    and item.color != [0, 0, 225]
+                    and item.color != [14, 253, 0]
+                    and item.color != [14, 253, 277]
+                ):
+                    object_list.remove(self)
+        super().update()
+
+
+class AltBreak(KineticBlock):
+    def update(self, object_list):
+        if self.touched_by_ball:
+            for item in object_list:
+                if item == self and item.color == [0, 0, 255]:
+                    self.color = [14, 253, 0]
+                    super().update()
+                if item == self and item.color == [14, 253, 0]:
+                    self.color = [14, 253, 277]
+                if item == self and item.color == [14, 253, 277]:
+                    object_list.remove(self)
+        super().update()
+
+
 class Paddle(KineticBlock):
     def update(self, left, right):
         if left:
-            self.rectangle.x += 3
+            self.position.x += 5
         if right:
-            self.rectangle.x -= 3
-        self.touched_by_ball = False
+            self.position.x -= 5
 
-
-class D_Blocks(KineticBlock):
-    # TODO
-    pass
+        self.rectangle = pygame.Rect(
+            self.position.x - (self.rectangle.width / 2),
+            self.position.y - (self.rectangle.height / 2),
+            self.rectangle.width,
+            self.rectangle.height,
+)
--- src/draw.py
+++ src/draw.py
@@ -1,98 +1,98 @@
+import sys
 import pygame  # TODO:  Fix intellisense
 import random
-import sys
 
 from pygame.math import Vector2
 
 from ball import *
-from block import *
+from block import Paddle, Breakable, AltBreak
 
-S_WIDTH = 400
-S_HEIGHT = 800
-
-SCREEN_SIZE = [S_WIDTH, S_HEIGHT]
-
+SCREEN_SIZE = [400, 800]
 BACKGROUND_COLOR = [255, 255, 255]
-PADDLE_WIDTH = 120
-PADDLE_HEIGHT = 30
-
-NUMBER_OF_BLOCKS = 3
-BLOCK_WIDTH = S_WIDTH / NUMBER_OF_BLOCKS
-BLOCK_HEIGHT = BLOCK_WIDTH / 2
-
-
-def Rnd_Num_gen():  # Random color blocks every game
-    return random.randint(0, 256)
 
 
 def debug_create_objects(object_list):
     kinetic = GameBall(
-        1,
-        object_list,
-        SCREEN_SIZE,
+        1,  # mass
+        object_list,  # object_list
+        SCREEN_SIZE,  # bounds
         Vector2(
             random.randint(20, SCREEN_SIZE[0] - 20),
             random.randint(20, SCREEN_SIZE[1] - 20),
-        ),
-        Vector2(4 * random.random() - 2, 4 * random.random() - 2),
-        [255, 10, 0],
-        20,
+        ),  # position
+        Vector2(4, 2),  # velocity
+        [255, 10, 0],  # color
+        20,  # radius
     )
     object_list.append(kinetic)
+    block = AltBreak(
+        Vector2(50, 20), 50, 20, [0, 0, 255]
+    )  # position width height color
+    object_list.extend((block,))
+    block = AltBreak(
+        Vector2(151, 20), 50, 20, [0, 0, 255]
+    )  # position width height color
+    object_list.extend((block,))
+    block = Breakable(Vector2(252, 20), 50, 20, [0, 0, 255])  # position width height color
+    object_list.extend((block,))
+    block = Breakable(Vector2(353, 20), 50, 20, [0, 0, 255])  # position width height color
+    object_list.extend((block,))
+    block = AltBreak(
+        Vector2(50, 80), 50, 20, [0, 0, 255]
+    )  # position width height color
+    object_list.extend((block,))
+    block = Breakable(Vector2(151, 80), 50, 20, [0, 0, 255])  # position width height color
+    object_list.extend((block,))
+    block = Breakable(Vector2(252, 80), 50, 20, [0, 0, 255])  # position width height color
+    object_list.extend((block,))
+    block = AltBreak(
+        Vector2(353, 80), 50, 20, [0, 0, 255]
+    )  # position width height color
+    object_list.extend((block,))
 
-    paddle = Paddle(
-        Vector2(S_WIDTH / 2, S_HEIGHT - 20), PADDLE_WIDTH, PADDLE_HEIGHT, [0, 0, 255]
-    )
-    object_list.append(paddle)
-    # CREATE BLOCKS
-
-    for i in range(NUMBER_OF_BLOCKS):
-        blocks = []
-        color = [Rnd_Num_gen(), Rnd_Num_gen(), Rnd_Num_gen()]
-        for x in range(3):
-            block = KineticBlock(
-                Vector2(74 + (i * 124), 100 + (x * 40)), 120, 30, color
-            )
-            object_list.append(block)
+    block = Paddle(Vector2(1, 799), 100, 50, [0, 0, 0])
+    object_list.extend((block,))
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
-
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
 
     object_list = []  # list of objects of all types in the toy
 
     debug_create_objects(object_list)
-
-    while True:  # TODO:  Create more elegant condition for loop
+    playing = True
+    while playing:  # TODO:  Create more elegant condition for loop
         left = False
         right = False
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
-        # TODO:  Feed input variables into update for objects that need it.
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             left = True
         if keys[pygame.K_RIGHT]:
             right = True
-        for object in object_list:
-            if isinstance(object, Paddle):
-                object.update(right, left)
-                object.check_collision()
+        for item in object_list:
+            if isinstance(item, Paddle):
+                item.update(right, left)
+            elif isinstance(item, Breakable) or isinstance(item, AltBreak):
+                item.update(object_list)
             else:
-                object.update()
-                object.check_collision()
+                item.update()
+            item.check_collision()
+            if hasattr(object, 'game_over'):
+                if object.game_over == True:
+                    print('GAME OVER')
+                    playing = False
 
         # Draw Updates
         screen.fill(BACKGROUND_COLOR)
         for ball in object_list:
             ball.draw(screen, pygame)
+
         clock.tick(60)
         pygame.display.flip()
 
